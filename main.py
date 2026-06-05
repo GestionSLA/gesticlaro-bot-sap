a607ab55f9dcf7e8fc19f94460f94435e2ba443a
Chev-Chellios <productousb@gmail.com>
2026-06-05 15:32:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def tarea_bot_sap(rango_inicio: str, rango_fin: str, SinUs: str, SinPass: str):
 
     try:
         print("Iniciando simulación del navegador... Abriendo SAP Fiori Claro")
-        driver.get("https://ondemand.com")
+        driver.get("https://flpnwc-d62f4ebf3.dispatcher.us2.hana.ondemand.com/sites/agentes#home-Display")
         time.sleep(12) 
 
         print("Paso 0: Desplegando formulario de login corporativo...")
